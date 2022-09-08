4ecf8d4a0dddc3401ab804b169048b1232067cd2
Abirami0906 <101399354+Abirami0906@users.noreply.github.com>
2022-09-08 16:48:33 +0530

    Create 02OddOrEven.java

---
+++ 02OddOrEven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sample{
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner (System.in);
+        int a=sc.nextInt();
+        if (a%2==0)
+        System.out.println(a+" IS AN EVEN NUMBER");
+        else
+System.out.println(a+"IS AN ODD NUMBER");
+        
+    }
+}

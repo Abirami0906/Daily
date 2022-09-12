8cd8af02b384635b9b8d58882dfa2e6fd5bad8aa
Abirami0906 <101399354+Abirami0906@users.noreply.github.com>
2022-09-12 07:44:18 +0530

    Create 06Candies.java

---
+++ 06Candies.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner (System.in);
+    int m=sc.nextInt();
+    int n=sc.nextInt();
+    if (m%n==0)
+    System.out.println("YES");
+    else 
+     System.out.println("NO");
+  }
+}

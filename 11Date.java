246a6ccaddbaed34d4984a486819635312afcaf3
Abirami0906 <101399354+Abirami0906@users.noreply.github.com>
2022-09-20 08:09:45 +0530

    Create 11Date.java

---
+++ 11Date.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+public class Main
+{
+    public static void main(String[] args)
+    {
+          LocalTime date = LocalTime.now();
+         System.out.println("The current time in 24 hour format is "+ date);
+    }
+}
